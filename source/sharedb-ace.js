fdae366521d156aa4daab22292944cf528a59b42
Jethro Kuan <jethrokuan95@gmail.com>
2017-01-15 14:51:49 +0800

    first passing test

--- source/sharedb-ace.js
+++ source/sharedb-ace.js
@@ -1,3 +1,23 @@
-(() => {
-  console.log("Hello");
-})();
+class sharedbAce {
+  /**
+   * @param aceInstance - Ace Editor Instance
+   * @param doc - ShareDB.Doc instance
+   */
+  constructor(aceInstance, doc) {
+    // Fetch Ace Editor Instance
+    this.session = aceInstance.getSession();
+    // If document is not initialized throw an error
+    if (doc.type === null)
+      throw new Error("Document Uninitialized");
+    else this.doc = doc; 
+  }
+
+  setup() {
+    this.session.setValue(this.doc.data);
+    doc.on('op', function(op, source) {
+      console.log(op);
+    });    
+  }
+}
+
+export default sharedbAce;
---
+++ test/headless.js
@@ -0,0 +1,46 @@
+require('amd-loader');
+var ShareDB = require('sharedb');
+var sharedbAce = require('../distribution/sharedb-ace').default;
+var assert = require('assert');
+
+// ShareDB setup
+var backend = new ShareDB();
+var connection = backend.connect();
+
+function newDoc(text, cb) {
+  var doc = connection.get('foo', 'bar'); 
+
+  doc.fetch(function(err) {
+    if (err) throw err;
+    if (doc.type === null) {
+      doc.create(text);
+      cb(doc); 
+    }
+  });
+}
+
+// Ace setup
+require('../ace/lib/ace/test/mockdom');
+var MockRenderer = require("../ace/lib/ace/test/mockrenderer").MockRenderer;
+var Editor = require("../ace/lib/ace/editor").Editor;
+var EditSession = require("../ace/lib/ace/edit_session").EditSession;
+
+function newSAEditor(doc) {
+  var editor = new Editor(new MockRenderer()); 
+  editor.$blockScrolling = Infinity; 
+  var session = new EditSession('');
+  editor.setSession(session);
+
+  var sa = new sharedbAce(editor, doc);
+  return sa;
+}
+
+describe('Editor creation', function() {
+  it('sets starting text', function() {
+    var sa; 
+    newDoc('hello', function() {
+      sa = newSAEditor(doc); 
+      assert.equal('hello', sa.session.getValue());
+    }); 
+  });
+});
