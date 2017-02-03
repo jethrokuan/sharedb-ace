0b53951b03cb648b263008aaeb9113c61a603ae9
Raynold Ng <raynold.ng24@gmail.com>
2017-02-03 11:14:26 +0800

    op to delta transform

--- source/sharedb-ace.js
+++ source/sharedb-ace.js
@@ -21,15 +21,16 @@ class sharedbAce {
       // false because op needs to be sent to the server
       self.doc.submitOp(op, false, function(err) {
         if (err) throw err;
-      }); 
+      });
     });
     // this.doc.on('op', function(op, source) {
     //   console.log(op);
     // });
   }
 
+
   /*
-   * Remote->Local changes
+   * Local -> Remote changes
    */
 
   deltaTransform(delta) {
@@ -46,7 +47,7 @@ class sharedbAce {
       } 
     } else if (delta.action === "remove") {
       if (delta.lines.length === 2) {
-        obj.sr = "\n"; 
+        obj.sr = "\n";
       } else {
         obj.sr = delta.lines[0];
       } 
@@ -55,10 +56,44 @@ class sharedbAce {
     }
     return obj;
   }
-  
   /*
+   [{"p":[4],"sd":"e"}]
+   [{"p":[4],"si":"d"}]
    * Local -> Remote changes
+   * Remote -> locate
+   Insert character:
+   {"start":{"row":5,"column":1},"end":{"row":5,"column":2},"action":"insert","lines":["d"]}
+   Insert new line:
+   {"start":{"row":7,"column":0},"end":{"row":8,"column":0},"action":"insert","lines":["",""]}
+   Delete line:
+
    */
+  /*
+   * Remote -> Local changes
+   */
+
+	opTransform(op) {
+    var index = aceInstance.session.doc.positionToIndex(op.path[0], 0);
+    var row = index.row;
+    var col = index.column;
+
+    if("sd" in op) {
+      // delete
+      lines = op.sd.split("\n");
+      delta = {"start":index,
+               "end":{"row":row + lines.length, "column":lines[lines.length - 1].length},
+               "action":"remove", "lines":lines};
+    } else if("si" in op) {
+      // insert
+      // get row and column
+      var lines = op.si.split("\n");
+      delta = {"start":index,
+               "end":{"row":row + lines.length, "column":lines[lines.length - 1].length},
+               "action":"insert", "lines":lines};
+
+    }
+	}
+
 }
 
 export default sharedbAce;
