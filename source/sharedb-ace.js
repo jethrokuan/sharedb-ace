7c3033dedf4d27835c04d66d3e432d9faf51a8b9
Jethro Kuan <jethrokuan95@gmail.com>
2017-02-06 20:45:41 +0800

    refactor op to delta

--- source/sharedb-ace.js
+++ source/sharedb-ace.js
@@ -8,16 +8,16 @@ class sharedbAce {
     this.session = aceInstance.getSession();
     // If document is not initialized throw an error
     // if (doc.type === null)
-    //   throw new Error("Document Uninitialized");
+    //   throw new Error('Document Uninitialized');
     // else this.doc = doc;
     this.setup();
   }
 
   setup() {
     // this.session.setValue(this.doc.data);
-    var self = this;
-    this.session.on("change", function(delta) {
-      var op = self.deltaTransform(delta);
+    const self = this;
+    this.session.on('change', function(delta) {
+      const op = self.deltaTransform(delta);
       console.log(op);
       // false because op needs to be sent to the server
       // self.doc.submitOp(op, false, function(err) {
@@ -31,35 +31,35 @@ class sharedbAce {
 
   /**
    * @param delta - delta that ace editor produces upon changes
-   * eg. {"start":{"row":5,"column":1},"end":{"row":5,"column":2},"action":"insert","lines":["d"]}
+   * eg. {'start':{'row':5,'column':1},'end':{'row':5,'column':2},'action':'insert','lines':['d']}
    */
   deltaTransform(delta) {
     // TODO: add path 
-    var aceDoc = this.session.getDocument();
-    var obj = {};
+    const aceDoc = this.session.getDocument();
+    const obj = {};
     obj.p = aceDoc.positionToIndex(delta.start);
-    var action;
-    if (delta.action === "insert") {
-      action = "si";
-    } else if (delta.action === "remove") {
-      action = "sr";
+    let action;
+    if (delta.action === 'insert') {
+      action = 'si';
+    } else if (delta.action === 'remove') {
+      action = 'sr';
     } else {
-      throw new Exception("action not supported");
+      throw new Exception('action not supported');
     }
     
-    var str = delta.lines.join("\n");
+    const str = delta.lines.join('\n');
     obj[action] = str;
     return obj;
   }
   /*
-    [{"p":[4],"sd":"e"}]
-    [{"p":[4],"si":"d"}]
+    [{'p':[4],'sd':'e'}]
+    [{'p':[4],'si':'d'}]
     * Local -> Remote changes
     * Remote -> locate
     Insert character:
-    {"start":{"row":5,"column":1},"end":{"row":5,"column":2},"action":"insert","lines":["d"]}
+    {'start':{'row':5,'column':1},'end':{'row':5,'column':2},'action':'insert','lines':['d']}
     Insert new line:
-    {"start":{"row":7,"column":0},"end":{"row":8,"column":0},"action":"insert","lines":["",""]}
+    {'start':{'row':7,'column':0},'end':{'row':8,'column':0},'action':'insert','lines':['','']}
     Delete line:
 
   */
@@ -68,25 +68,27 @@ class sharedbAce {
    */
 
 	opTransform(op) {
-    var index = aceInstance.session.doc.positionToIndex(op.path[0], 0);
-    var row = index.row;
-    var col = index.column;
-
-    if("sd" in op) {
-      // delete
-      lines = op.sd.split("\n");
-      delta = {"start":index,
-               "end":{"row":row + lines.length, "column":lines[lines.length - 1].length},
-               "action":"remove", "lines":lines};
-    } else if("si" in op) {
-      // insert
-      // get row and column
-      var lines = op.si.split("\n");
-      delta = {"start":index,
-               "end":{"row":row + lines.length, "column":lines[lines.length - 1].length},
-               "action":"insert", "lines":lines};
-
+    const pos = op.path[op.path.length -1];
+    const index = aceInstance.session.doc.positionToIndex(pos, 0);
+    let action;
+    if('sd' in op) {
+      action = 'remove';
+    } else if ('si' in op) {
+      action = 'insert';
+    } else {
+      throw Exception('Invalid Operation: ' + JSON.stringify(op));
     }
+    const lines = op[action].split('\n');
+    const delta = {
+      'start': index,
+      'end':{
+        'row': index.row + lines.length,
+        'column': lines[lines.length - 1].length
+      },
+      'action': action,
+      'lines': lines
+    };
+    return delta;
 	}
 
 }
