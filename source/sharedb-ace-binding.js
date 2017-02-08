0dcd6c8f21da1707f4b8f50597d2768d06b05e17
Jethro Kuan <jethrokuan95@gmail.com>
2017-02-08 08:54:58 +0800

    Fix ESLint Errors

--- source/sharedb-ace-binding.js
+++ source/sharedb-ace-binding.js
@@ -1,13 +1,13 @@
-class sharedbAceBinding {
+class SharedbAceBinding {
   constructor(aceInstance, path, doc) {
     this.editor = aceInstance;
     this.editor.$blockScrolling = Infinity;
     this.session = aceInstance.getSession();
-    this.newline = this.session.getDocument().getNewLineCharacter(); 
+    this.newline = this.session.getDocument().getNewLineCharacter();
     this.path = path;
     this.doc = doc;
-    this.suppress = false; 
-    this.setup(); 
+    this.suppress = false;
+    this.setup();
   }
 
   setup() {
@@ -20,18 +20,18 @@ class sharedbAceBinding {
     self.session.removeAllListeners('change');
     self.doc.removeAllListeners('op');
     // self.session.on('change', self.onLocalChange.call(self));
-      
+
     self.session.on('change', self.onLocalChange.bind(self));
-    
-    self.doc.on('op', self.onRemoteChange.bind(self)); 
+
+    self.doc.on('op', self.onRemoteChange.bind(self));
   }
-  
+
   /**
    * @param delta - delta that ace editor produces upon changes
    * eg. {'start':{'row':5,'column':1},'end':{'row':5,'column':2},'action':'insert','lines':['d']}
    */
   deltaTransform(delta) {
-    const aceDoc = this.session.getDocument(); 
+    const aceDoc = this.sfession.getDocument();
     const op = {};
     op.p = this.path.concat(aceDoc.positionToIndex(delta.start));
     let action;
@@ -40,9 +40,9 @@ class sharedbAceBinding {
     } else if (delta.action === 'remove') {
       action = 'sd';
     } else {
-      throw `action ${action} not supported`;
+      throw new Error(`action ${action} not supported`);
     }
-    
+
     const str = delta.lines.join('\n');
     console.log(JSON.stringify(str));
     op[action] = str;
@@ -55,82 +55,80 @@ class sharedbAceBinding {
    */
   opTransform(op) {
     const self = this;
-    const index = op.p[op.p.length -1]; 
-    const pos = self.session.doc.indexToPosition(index, 0); 
-    let action, lines;
-    
-    if('sd' in op) {
+    const index = op.p[op.p.length - 1];
+    const pos = self.session.doc.indexToPosition(index, 0);
+    let action;
+    let lines;
+
+    if ('sd' in op) {
       action = 'remove';
       lines = op.sd.split('\n');
     } else if ('si' in op) {
       action = 'insert';
       lines = op.si.split('\n');
     } else {
-      throw Exception('Invalid Operation: ' + JSON.stringify(op));
+      throw new Error(`Invalid Operation: ${JSON.stringify(op)}`);
     }
 
     let count = 0;
-    for (let line in lines) {
+    for (const line of lines) {
       count += lines[line].length;
     }
     count += lines.length - 1;
-    
+
     const start = pos;
     const end = self.session.doc.indexToPosition(index + count, 0);
-    
+
     const delta = {
-      'start': start,
-      'end': end,
-      'action': action,
-      'lines': lines
+      start,
+      end,
+      action,
+      lines,
     };
     return delta;
   }
 
   onLocalChange(delta) {
-    console.log("local change event fired");
-    console.log(delta);
-    const self = this; 
+    const self = this;
 
     // Rerender the whole document
     self.repaint();
 
     // self.editor._signal("change", delta);
-    
+
     // Update cursor because tab characters can influence the cursor position.
     self.editor.$cursorChange();
-    self.editor.$updateHighlightActiveLine(); 
+    self.editor.$updateHighlightActiveLine();
 
     if (self.suppress) return;
-    
     const op = self.deltaTransform(delta);
 
-    self.doc.submitOp(op, {source: self}, function(err) {
-      if (err) throw err; 
-    });
+    const docSubmitted = (err) => {
+      if (err) throw err;
+    };
+
+    self.doc.submitOp(op, { source: self }, docSubmitted);
   }
 
   // Repaint the whole document
   // TODO: optimize, only repaint from start row onwards
   repaint() {
-    var wrap = this.editor.session.$useWrapMode;
-    var lastRow = this.session.getDocument().getLength() -1;
+    const wrap = this.editor.session.$useWrapMode;
+    const lastRow = this.session.getDocument().getLength() - 1;
     console.log(`Repainting: lines 0 to ${lastRow}`);
     this.editor.renderer.updateLines(0, lastRow, wrap);
   }
 
   onRemoteChange(ops, source) {
-    console.log("remove event fired");
     const self = this;
+    console.log('remove event fired');
     if (source === self) return;
     const deltas = [];
-    
     for (const op of ops) {
       deltas.push(self.opTransform(op));
     }
-    
     self.suppress = true;
-    self.session.getDocument().applyDeltas(deltas); 
+    self.session.getDocument().applyDeltas(deltas);
     self.suppress = false;
     self.repaint();
   }
--- source/sharedb-ace.js
+++ source/sharedb-ace.js
@@ -1,14 +1,16 @@
-var sharedb = require("../node_modules/sharedb/lib/client");
-import sharedbAceBinding from "./sharedb-ace-binding";
-import EventEmitter from "event-emitter-es6";
+import EventEmitter from 'event-emitter-es6';
+import sharedb from '../node_modules/sharedb/lib/client';
 
-class sharedbAce extends EventEmitter {
+import SharedbAceBinding from './sharedb-ace-binding';
+
+
+class SharedbAce extends EventEmitter {
 
   /**
    * @param wsUrl - URL to connect to shareDB
    * @param namespace - database table
    * @param id - document id
-   * 
+   *
    * creating a sharedbAce instance connects to sharedb via the websocket URL
    * and initiates the document
    * and initializes the sharedb with no connections
@@ -20,28 +22,31 @@ class sharedbAce extends EventEmitter {
     const connection = new sharedb.Connection(socket);
     const doc = connection.get(namespace, id);
     // Fetches once from the server, and fires events on subsequent document changes
-    doc.subscribe(function(err) {
+
+    const docSubscribed = (err) => {
       if (err) throw err;
       if (doc.type === null) {
-        throw 'Document Uninitialized';
+        throw new Error('Document Uninitialized');
       }
       self.emit('ready');
-    }); 
+    };
+
+    doc.subscribe(docSubscribed);
     self.doc = doc;
     self.connections = {};
   }
-  
+
   /**
    * @param aceInstance - Ace Editor Instance
    * @param doc - ShareDB.Doc instance
    * adds a two-way binding between an aceInstance and the path
-   * eg. add(aceInstance, ["foo"]);
-   */ 
-  add(session, path) {
-    var binding = new sharedbAceBinding(session, path, this.doc);
-    this.connections[path.join(',')] = binding; 
+   * eg. add(aceInstance, ['foo']);
+   */
+  add(aceInstance, path) {
+    const binding = new SharedbAceBinding(aceInstance, path, this.doc);
+    this.connections[path.join(',')] = binding;
     binding.setup();
   }
 }
 
-export default sharedbAce;
+export default SharedbAce;
